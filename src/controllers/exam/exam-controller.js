7216b1bfcec4ed4452df953994580f945713769e
ZerOne <whoamixzerone@gmail.com>
2022-07-12 20:49:55 +0900

    [Feat] initialize

--- src/controllers/exam/exam-controller.js
+++ src/controllers/exam/exam-controller.js
@@ -2,7 +2,8 @@ import statusCode from './../../utils/status-code.js';
 import message from './../../utils/response-message.js';
 
 const test = (req, res) => {
-  res.status(statusCode.OK).send(message.SUCCESS);
+  // res.status(statusCode.OK).send(message.SUCCESS);
+  res.status(200).send('Hello World!');
 };
 
 export default {
