8d5fddce9c372c35f6a37839316ad83f436a79b6
juhwano <calculus9006@gmail.com>
2022-07-14 14:55:56 +0900

    [Test] Success Test

--- src/test/exam/exam.test.js
+++ src/test/exam/exam.test.js
@@ -3,15 +3,7 @@ describe('test', () => {
     expect(1 + 1).toBe(2);
   });
 
-  test('1+2', () => {
-    expect(1 + 2).toBe(3);
-  });
-
-  test('2+4', () => {
-    expect(2 + 4).toBe(5);
-  });
-
   test('3+4', () => {
-    expect(3 + 4).toBe(6);
+    expect(3 + 4).toBe(7);
   });
 });
